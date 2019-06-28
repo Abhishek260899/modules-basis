c53ee17b1fd70a948a511b48459142fe36e7a71a
Abhishek260899 <51901519+Abhishek260899@users.noreply.github.com>
2019-06-28 15:16:02 +0530

    cards updated

--- Cards.js
+++ Cards.js
@@ -5,12 +5,9 @@ class Cards extends React.Component{
     constructor(){
         super()
         this.state={
-            Data:{},
-            cardItem:[]
         }
     }
     componentDidMount(){
-        
         this.props.selcards.sort(this.compare)
         console.log(this.props.selcards)
         var bearer='Bearer 5d0ba2fa6bcfc87139e85170,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkMGJhMmZhNmJjZmM4NzEzOWU4NTE3MCIsImlhdCI6MTU2MTM3MTEyNCwiZXhwIjoxNTYxOTc1OTI0fQ.IazTLyTq-9_g6MxqGnJt0zSCbY4l__aqEVVLxajpu7E';
@@ -25,31 +22,55 @@ class Cards extends React.Component{
                 'X-FP-API-KEY': 'iphone', 
                 'Content-Type': 'application/json'
         }}).then(response =>response.json()).then(data =>{
-             console.log(data);
+             
               this.setState({
                    Data:data,
                    cardItem:data.results.cards
               })
         })
     }
-    
+    componentDidUpdate(prevProps,prevState){
+        if(prevProps.selcards !==this.props.selcards){
+        this.props.selcards.sort(this.compare)
+        console.log(this.props.selcards)
+        var bearer='Bearer 5d0ba2fa6bcfc87139e85170,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkMGJhMmZhNmJjZmM4NzEzOWU4NTE3MCIsImlhdCI6MTU2MTM3MTEyNCwiZXhwIjoxNTYxOTc1OTI0fQ.IazTLyTq-9_g6MxqGnJt0zSCbY4l__aqEVVLxajpu7E';
+
+        fetch("https://dev.getbasis.co/v1/modules/cards",{
+            method: 'POST',
+            withCredentials: true,
+           body: JSON.stringify({ cards:this.props.selcards}),
+        
+            headers:{
+                'Authorization': bearer,
+                'X-FP-API-KEY': 'iphone', 
+                'Content-Type': 'application/json'
+        }}).then(response =>response.json()).then(data =>{
+             
+              this.setState({
+                   Data:data,
+                   cardItem:data.results.cards
+              })
+        })
+    }
+    }    
      compare(a,b){
        const c=a.position
        const d=b.position
        return c-d
     }
+
     render(){
-     console.log(this.state.Data)
+     
   
       if(this.state.Data===undefined){
           return(<div> </div>)
       }
       
-      
+ 
       return(
         <div>
            {this.state.cardItem.map((mod,index) =><div><p key={index}>{mod.title}</p>
-           <p key={index}>{ReactHtmlParser(mod.content)} </p></div>)}
+           <p key={mod._id}>{ReactHtmlParser(mod.content)} </p></div>)}
             </div>   
       )
     }
